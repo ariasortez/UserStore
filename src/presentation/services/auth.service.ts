4c4ebe5bf15da4f5fb194ae19c6132764a21ccd3
ariasortez <juan.arias@unitec.edu>
2024-07-11 19:13:57 -0600

    AuthService Validation Completed

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { UserModel } from '../../data';
-import { CustomError, RegisterUserDOT } from '../../domain';
+import { CustomError, RegisterUserDOT, UserEntity } from '../../domain';
 
 export class AuthService {
   constructor() {}
@@ -8,6 +8,16 @@ export class AuthService {
     const existUser = await UserModel.findOne({ email: registerUserDto.email });
 
     if (existUser) throw CustomError.badRequest('Email already exist');
+
+    try {
+      const user = new UserModel(registerUserDto);
+      await user.save();
+
+      const { password, ...newUser } = UserEntity.fromObject(user);
+      return {
+        user: newUser,
+      };
+    } catch (error) {}
 
     return 'Todo ok';
   }
